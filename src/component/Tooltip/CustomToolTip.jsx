9891774096c77c689b40204b6b62183bda53ebb3
sudalqueen <abbey17@cnu.ac.kr>
2020-07-16 16:46:49 +0900

    Add CustomTooltip
    Add CustomTooltip for usage example

---
+++ src/component/Tooltip/CustomToolTip.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DefaultToolTip = styled.div`
+margin-bottom: 5px;
+padding: 10px;
+width: 80px;
+-webkit-border-radius: 3px;
+-moz-border-radius: 3px;
+border-radius: 15px;
+background-color: #61dafb;
+background-color: hsla(#61dafb, 0%, 20%, 0.9);
+color: #fff;
+content: attr(data-tooltip);
+text-align: center;
+font-size: 16px;
+line-height: 1.2;
+&: after{
+    position: absolute;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -5px;
+  width: 0;
+  border-bottom: 5px solid #61dafb;
+  border-bottom: 5px solid hsla(#61dafb, 0%, 20%, 0.9);
+  border-right: 5px solid transparent;
+  border-left: 5px solid transparent;
+  content: '\u25b2';
+  font-size: 0;
+  line-height: 0;
+  -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+  filter: progid: DXImageTransform.Microsoft.Alpha(Opacity=0);
+  pointer-events: none;
+}
+`;
+
+function CustomTooltip(props) {
+    return (
+        <DefaultToolTip>
+            {props.children}
+        </DefaultToolTip>
+    )
+}
+
+export default CustomTooltip;
