a4e9a9a5d69b74d362041d6107618fcdb79b5da5
Sudal <abbey17@naver.com>
2020-07-24 09:23:44 +0900

    Edit App.js
    Add usecase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import WordView from "./component/WordTagger";
+import TagEditor from './component/TagEditor';
+import CustomTooltip from './component/Tooltip/CustomToolTip'
 
 function App() {
+  const map = new Map();
+
+  const tags = [
+    { name: "Name" },
+    { name: "Gender" }
+  ]
+
+  const tags2 = [
+    { name: "Fruits" },
+    { name: "Nothing!" }
+  ]
+
+  function onChange(info) {
+    console.log(info);
+  }
+
+  function onClickTag(name, word, tagged) {
+    const arr = map.get(name) || [];
+
+    if (tagged) {
+      arr.push(word);
+    } else {
+      const index = arr.indexOf(word);
+      arr.splice(index, 1);
+    }
+    console.log(name, word, tagged)
+  }
+
   return (
     <div className="App">
-      <WordView/>
+      <TagEditor
+        value="test text"
+        tags={tags}
+        divClassName={"firstTag"}
+        onChange={onChange}
+        onClickTag={onClickTag}
+        customTooltip={CustomTooltip} />
+      <br />
+      <TagEditor value="This is apple, banana, orange!" tags={tags2} />
     </div>
   );
 }
